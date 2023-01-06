d529883a1adcc004a8472e2f62b767fbbb20129c
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2023-01-06 15:28:55 +0000

    Include todo and autodoc Sphinx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,12 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser', 'sphinxcontrib.opendataservices', 'sphinxcontrib.jsonschema', 'sphinx_rtd_theme',]
+extensions = ['myst_parser',
+              'sphinxcontrib.opendataservices',
+              'sphinxcontrib.jsonschema',
+              'sphinx_rtd_theme',
+              'sphinx.ext.todo',
+              'sphinx.ext.autodoc',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
